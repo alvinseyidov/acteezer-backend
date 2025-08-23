592186a36e43f0008f0d4bb6afd29581f6126cdb
Elvin Seyidov <alvinseyidov@gmail.com>
2025-08-23 12:03:10 +0400

    update

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-u7sp=nd-m-9^xe-95bc3-#sm35c7t*&*@23^d_+dsqbi5#9uk5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
